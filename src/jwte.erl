b9fcd69f30d0a4713c04356208ef11c1b7d68d27
Thomas Bhatia <thomas.bhatia@eo.io>
2017-01-17 21:28:31 +0000

    Map checkpoint

--- src/jwte.erl
+++ src/jwte.erl
@@ -17,32 +17,29 @@ peek(JWT) ->
     verify(JWT, undefined).
 
 sign(Claims, Key) ->
-    sign(Claims, Key, <<"HS256">>).
+    sign(#{claims => Claims, key => Key}).
 
-sign(Claims, Key, Algorithm) ->
-    Header = [{<<"alg">>, Algorithm}, {<<"typ">>, <<"JWT">>}],
+sign(Claims, Key, Alg) ->
+    sign(#{claims => Claims, key => Key, alg => Alg}).
+
+sign(#{claims := _Claims, key := _Key} = Payload0) when (map_size(Payload0) =:= 2) ->
+    Payload = Payload0#{alg => <<"HS256">>},
+    sign(Payload);
+
+sign(#{claims := Claims, key := Key, alg := Alg}) ->
+    Header = [{<<"alg">>, Alg}, {<<"typ">>, <<"JWT">>}],
     Hjson = jsx:encode(Header),
     Pjson = jsx:encode(Claims),
     Hb = base64url:encode(Hjson),
     Pb = base64url:encode(Pjson),
     UnsignedToken = <<Hb/binary, ".", Pb/binary>>,
-    Signed = base64url:encode(signer(get_signature_type(Algorithm), Key, UnsignedToken)),
+    SigType = get_signature_type(Alg),
+    Signer = signer(SigType, Key, UnsignedToken),
+    Signed = base64url:encode(Signer),
     <<UnsignedToken/binary, ".", Signed/binary>>.
 
-% sign(#{claims := Claims, key := Key, alg := Alg} = Map0) ->
-%     Map = Map0#{
-%             header = [{<<"alg">>, Algorithm}, {<<"typ">>, <<"JWT">>}],
-%             hjson = jsx:
-%         },
-%     Header = [{<<"alg">>, Algorithm}, {<<"typ">>, <<"JWT">>}],
-%     Hjson = jsx:encode(Header),
-%     Pjson = jsx:encode(Claims),
-%     Hb = base64url:encode(Hjson),
-%     Pb = base64url:encode(Pjson),
-%     UnsignedToken = <<Hb/binary, ".", Pb/binary>>,
-%     Signed = base64url:encode(signer(get_signature_type(Algorithm), Key, UnsignedToken)),
-%     <<UnsignedToken/binary, ".", Signed/binary>>.
 
+%%% Verify %%%
 verify(JWT, Key) ->
     verify(JWT, Key, <<"HS256">>).
 
@@ -75,6 +72,7 @@ verify([_Verified, false, _Payload]) ->
 signer({hmac, Algorithm}, Secret, UnsignedToken) ->
     Digest = get_hash_algorithm(Algorithm),
     crypto:hmac(Digest, Secret, UnsignedToken);
+
 signer({rsa, Algorithm}, PrivateKeyPem, UnsignedToken) when is_binary(PrivateKeyPem) ->
     [RSAEntry] = public_key:pem_decode(PrivateKeyPem),
     Key = public_key:pem_entry_decode(RSAEntry),
@@ -126,4 +124,5 @@ get_signature_type({_Title,AlgorithmBin,_HashAlgorithm,SignatureType}, _Bin) ->
     {SignatureType, AlgorithmBin};
 get_signature_type(false, Bin) ->
     erlang:error({badarg, Bin}).
+
 
